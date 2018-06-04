d8042b6efb8f11aa66256248e8bfc1b283d81b08
tufahija <musaka.tufahija@gmail.com>
2018-06-04 14:34:40 +0200

    good_start_with_button

--- app/src/main/java/com/tufahija/swipe/fragments/SwipeFragment1.java
+++ app/src/main/java/com/tufahija/swipe/fragments/SwipeFragment1.java
@@ -1,6 +1,7 @@
 package com.tufahija.swipe.fragments;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.tufahija.swipe.R;
@@ -18,7 +20,9 @@ import butterknife.OnClick;
 
 public class SwipeFragment1 extends Fragment {
     @BindView(R.id.textViewSF) TextView textViewCaption;
+    @BindView(R.id.buttonSF1small) Button buttonSmall;
     private View layoutView;
+    boolean captionIsBig = true;
 
     @Nullable
     @Override
@@ -35,5 +39,24 @@ public class SwipeFragment1 extends Fragment {
     @OnClick(R.id.buttonSFInsert)
     public void paintCaption(){
         layoutView.findViewById(R.id.textViewSF).setBackgroundColor(Color.RED);
+    }
+    @OnClick(R.id.buttonSF1small)
+    public void smallCaption(){
+
+        if (captionIsBig){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textViewCaption.setTextAppearance(R.style.TextAppearance_AppCompat_Body1);
+            }
+            captionIsBig = false;
+            buttonSmall.setText(R.string.big_caption);
+        }else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textViewCaption.setTextAppearance(R.style.TextAppearance_AppCompat_Display1);
+            }
+            captionIsBig = true;
+            buttonSmall.setText(R.string.small_caption);
+        }
+
+
     }
 }
